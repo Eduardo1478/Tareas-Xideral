1f621ef8a86d4bd2703c9b772cf8ab5567b32bce
Eduardo1478 <jepf2001@hotmail.com>
2024-09-08 23:33:17 -0600

    Tests

--- Semana4/rest_api_jpa-mockito/src/main/java/com/example/shopping_cart_rest_api_jpa/controllers/Controllertest.java
+++ Semana4/rest_api_jpa-mockito/src/main/java/com/example/shopping_cart_rest_api_jpa/controllers/Controllertest.java
@@ -1,24 +1,25 @@
 package com.example.shopping_cart_rest_api_jpa.controllers;
 
-import com.example.shopping_cart_rest_api_jpa.models.Item;
-import com.example.shopping_cart_rest_api_jpa.repository.ItemRepository;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.example.shopping_cart_rest_api_jpa.models.Item;
+import com.example.shopping_cart_rest_api_jpa.repository.ItemRepository;
 
 public class Controllertest {
 
@@ -39,14 +40,15 @@ public class Controllertest {
     @Test
     void testGetItems() throws Exception{
         List<Item> mockItems = Arrays.asList(
-                new Item(1L, "Item1", 100.0, "Section1", 1.5, "2024-01-01"),
-                new Item(2L, "Item2", 150.0, "Section2", 2.0, "2024-02-01")
+            new Item(1L, "Item1", 100.0, LocalDate.of(2024, 1, 1), "Section1", 1.5),
+            new Item(2L, "Item2", 150.0, LocalDate.of(2024, 2, 1), "Section2", 2.0)
         );
         when(repository.findAll()).thenReturn(mockItems);
 
-        mockMvc.perform(get("/items"))
-            .contentType(MediaType.APPLICATION_JSON)
+        mockMvc.perform(MockMvcRequestBuilders.get("/items")
+            .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk());
+
         
         verify(repository, times(1)).findAll();
     }
